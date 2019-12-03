bafbc61f760045e9ee1e5cb2e154cc178d588a2e
Dat Duong <datd1988@gmail.com>
2019-12-03 04:00:17 -0800

    testing

---
+++ MakeData/ExampleCode/Command.sh
@@ -0,0 +1,7 @@
+
+java -jar psl-cli-2.1.0.jar --learn --model rule.psl --data input.data
+
+
+java -jar psl-cli-2.1.0.jar --learn --model preference-prediction.psl --data preference-prediction-learn.data
+
+
