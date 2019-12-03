d86f6fc460d520be5007bada988c909b49562ae5
Dat Duong <datd1988@gmail.com>
2019-12-03 04:49:02 -0800

    fix tab

--- MakeData/ExampleCode/Command.sh
+++ MakeData/ExampleCode/Command.sh
@@ -1,7 +1,12 @@
 
 java -jar psl-cli-2.1.0.jar --learn --model rule.psl --data input.data
 
+java -jar psl-cli-2.1.0.jar --infer --model rule-learned.psl --data input.data --output scratch_testing
 
-java -jar psl-cli-2.1.0.jar --learn --model preference-prediction.psl --data preference-prediction-learn.data
+
+# java -jar psl-cli-2.1.0.jar --learn --model preference-prediction.psl --data preference-prediction-learn.data
 
 
+for file in Subset Label Classifier ClassifierPredict Rating_target Rating_truth ; do
+perl -p -i -e 's/ /\t/g' $file.txt
+done
