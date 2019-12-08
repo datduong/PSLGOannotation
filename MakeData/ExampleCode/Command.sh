0d6f3d51333c31f144872b52cb53d3484ac907f1
Dat Duong <datd1988@gmail.com>
2019-12-07 23:14:41 -0800

    testing some cases

--- MakeData/ExampleCode/Command.sh
+++ MakeData/ExampleCode/Command.sh
@@ -1,4 +1,10 @@
 
+
+
+for file in SimilarLabel Subset Label ErrorLink ClassifierLink Classifier ClassifierPredict Rating_target Rating_truth ; do
+perl -p -i -e 's/ /\t/g' $file.txt
+done
+
 java -jar psl-cli-2.1.0.jar --learn --model rule.psl --data input.data
 
 java -jar psl-cli-2.1.0.jar --infer --model rule-learned.psl --data input.data --output scratch_testing
@@ -6,7 +12,3 @@ java -jar psl-cli-2.1.0.jar --infer --model rule-learned.psl --data input.data -
 
 # java -jar psl-cli-2.1.0.jar --learn --model preference-prediction.psl --data preference-prediction-learn.data
 
-
-for file in Subset Label Classifier ClassifierPredict Rating_target Rating_truth ; do
-perl -p -i -e 's/ /\t/g' $file.txt
-done
