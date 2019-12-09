4da2cd59fa556da4c0930e781db990290aaeec5d
Dat Duong <datd1988@gmail.com>
2019-12-09 04:37:50 -0800

    fix to take arg

--- MakeData/FormatData/SimilarLabel/FormatLabel.py
+++ MakeData/FormatData/SimilarLabel/FormatLabel.py
@@ -16,40 +16,58 @@ from tqdm import tqdm
 
 from scipy.spatial.distance import cosine
 
-os.chdir("/u/scratch/d/datduong/deepgo/data")
-OUTPUT_PATH = 'PSLdata'
-if not os.path.exists(OUTPUT_PATH): 
-  os.mkdir(OUTPUT_PATH)
+def run (WORKDIR,OUTPUT_PATH,LABEL_SEEN,LABEL_COMPLETE,ONTO) :
 
-graph = obonet.read_obo('go.obo')
+  # WORKDIR = "/u/scratch/d/datduong/deepgo/data"
+  os.chdir(WORKDIR)
+  # OUTPUT_PATH = 'PSLdata'
+  if not os.path.exists(OUTPUT_PATH):
+    os.mkdir(OUTPUT_PATH)
 
-label_names_seen = pd.read_csv("deepgo.all.terms.seen.csv",sep="\t",header=None)
-label_names_seen = sorted (list(label_names_seen[0]))
+  graph = obonet.read_obo('go.obo')
 
-label_vec = pickle.load(open("cosine.AveWordClsSep768.Linear768.Layer12/label_vector.pickle","rb"))
-label_names_complete = sorted(list(label_vec.keys()))
-print (label_names_complete[0:10])
+  label_names_seen = pd.read_csv(LABEL_SEEN,sep="\t",header=None) ## "deepgo.all.terms.seen.csv"
+  label_names_seen = sorted (list(label_names_seen[0]))
 
-os.chdir(OUTPUT_PATH)
+  ## COMMENT these are labels to be predicted
+  label_names_complete = pd.read_csv(LABEL_COMPLETE,sep="\t",header=None) ## "deepgo.all.terms.seen.csv"
+  label_names_complete = sorted (list(label_names_complete[0]))
 
-## COMMENT create subset file, parent \t child \t 1
-fout = open("Subset.txt","w")
-for index1, value1 in enumerate(label_names_complete):
-  if value1 in ['GO:0008150','GO:0003674','GO:0005575']: ## don't need the roots when compute similarity
-    continue
-  # Find edges to parent terms
-  for child, parent, key in graph.out_edges(value1, keys=True):
-    if key == 'is_a':
-      fout.write(parent+"\t"+child+"\t1.0\n")
-fout.close()
+  ## COMMENT filter by ontology ?
+  if ONTO is not 'none':
+    label_names_seen = [ l for l in label_names_seen if graph.node[l]['namespace'] != ONTO ]
+    label_names_complete = [ l for l in label_names_complete if graph.node[l]['namespace'] != ONTO ]
+
+
+  os.chdir(OUTPUT_PATH)
+
+  ## COMMENT create subset file, parent \t child \t 1
+  fout = open("Subset.txt","w")
+  for index1, value1 in enumerate(label_names_complete):
+    if value1 in ['GO:0008150','GO:0003674','GO:0005575']: ## don't need the roots when compute similarity
+      continue
+    # Find edges to parent terms
+    for child, parent, key in graph.out_edges(value1, keys=True):
+      if key == 'is_a':
+        fout.write(parent+"\t"+child+"\t1.0\n")
+  fout.close()
 
   ## COMMENT write out which label we have seen, and not
-foutLabel = open("Label.txt","w")
-for index1, value1 in enumerate(label_names_complete):
-  if value1 in label_names_seen:
-    foutLabel.write(value1+"\t"+"1.0\n") ## WE SEE THIS LABEL
-  else:
-    foutLabel.write(value1+"\t"+"0.0\n")
-foutLabel.close()
+  foutLabel = open("Label.txt","w")
+  for index1, value1 in enumerate(label_names_complete):
+    if value1 in label_names_seen:
+      foutLabel.write(value1+"\t"+"1.0\n") ## WE SEE THIS LABEL
+    else:
+      foutLabel.write(value1+"\t"+"0.0\n")
+  foutLabel.close()
+
+
+
+if len(sys.argv)<1: ## run script
+	print("Usage: \n")
+	sys.exit(1)
+else:
+  # WORKDIR,OUTPUT_PATH,LABEL_SEEN,LABEL_COMPLETE,ONTO
+	run ( sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5] )
 
 
--- MakeData/FormatData/SimilarLabel/FormatSimilarLabel.py
+++ MakeData/FormatData/SimilarLabel/FormatSimilarLabel.py
@@ -14,65 +14,86 @@ import obonet
 from tqdm import tqdm
 from sklearn.metrics import pairwise
 
-os.chdir("/u/scratch/d/datduong/deepgo/data")
-OUTPUT_PATH = 'PSLdata'
-if not os.path.exists(OUTPUT_PATH):
-  os.mkdir(OUTPUT_PATH)
+def run (WORKDIR,OUTPUT_PATH,LABEL_SEEN,LABEL_COMPLETE,LABEL_VECTOR,ONTO) :
 
-graph = obonet.read_obo('go.obo')
+  # WORKDIR = "/u/scratch/d/datduong/deepgo/data"
+  os.chdir(WORKDIR)
+  # OUTPUT_PATH = 'PSLdata'
+  if not os.path.exists(OUTPUT_PATH):
+    os.mkdir(OUTPUT_PATH)
 
-label_names_seen = pd.read_csv("deepgo.all.terms.seen.csv",sep="\t",header=None)
-label_names_seen = sorted (list(label_names_seen[0]))
+  graph = obonet.read_obo('go.obo')
 
-## COMMENT in @data, it doesn't really matter, just call the vectors only, same vectors even for larger dataset.
-label_vec = pickle.load(open("/u/scratch/d/datduong/deepgo/data/cosine.AveWordClsSep768.Linear768.Layer12/label_vector.pickle","rb"))
-label_names_complete = sorted(list(label_vec.keys()))
-print (label_names_complete[0:10])
+  label_names_seen = pd.read_csv(LABEL_SEEN,sep="\t",header=None) ## "deepgo.all.terms.seen.csv"
+  label_names_seen = sorted (list(label_names_seen[0]))
 
-label_vec_matrix = np.zeros( (len(label_names_complete),768) )
-for index1, value1 in enumerate(label_names_complete):
-  label_vec_matrix[index1] = label_vec[value1]
+  ## COMMENT these are labels to be predicted
+  label_names_complete = pd.read_csv(LABEL_COMPLETE,sep="\t",header=None) ## "deepgo.all.terms.seen.csv"
+  label_names_complete = sorted (list(label_names_complete[0]))
 
-label_index_map = {value1 : index1 for index1, value1 in enumerate(label_names_complete)}
-index_label_map = {index1 : value1 for index1, value1 in enumerate(label_names_complete)}
+  ## COMMENT filter by ontology ?
+  if ONTO is not 'none':
+    label_names_seen = [ l for l in label_names_seen if graph.node[l]['namespace'] != ONTO ]
+    label_names_complete = [ l for l in label_names_complete if graph.node[l]['namespace'] != ONTO ]
 
-sim = pairwise.cosine_similarity(label_vec_matrix) ## possible ??
+  label_names_complete = sorted ( list ( set (label_names_complete + label_names_seen) ) ) ## make sure we have all @label_names_seen into the list to be predicted as well
+  print ('total label to be predicted by PSL {}'.format(len(label_names_complete)))
 
-for onto in ['cellular_component','molecular_function','biological_process']:
-  OUTPUT_PATH2 = os.path.join(OUTPUT_PATH, onto)
+  # label_index_map = {value1 : index1 for index1, value1 in enumerate(label_names_complete)}
+  index_label_map = {index1 : value1 for index1, value1 in enumerate(label_names_complete)}
+
+  ## COMMENT word vectors
+  label_vec = pickle.load(open(LABEL_VECTOR,"rb")) # "/u/scratch/d/datduong/deepgo/data/cosine.AveWordClsSep768.Linear768.Layer12/label_vector.pickle"
+  names_in_label_vec = sorted(list(label_vec.keys())) ## @names_in_label_vec is needed to retain correct order
+  label_vec_matrix = np.zeros( (len(names_in_label_vec),768) )
+  for index1, value1 in enumerate(names_in_label_vec):
+    label_vec_matrix[index1] = label_vec[value1]
+
+  sim = pairwise.cosine_similarity(label_vec_matrix) ## possible ??
+
+  # for ONTO in ['cellular_component','molecular_function','biological_process']:
+
+  OUTPUT_PATH2 = os.path.join(OUTPUT_PATH, ONTO)
   if not os.path.exists(OUTPUT_PATH2):
     os.makedirs (OUTPUT_PATH2)
 
   os.chdir(OUTPUT_PATH2)
 
-  ## COMMENT filter by ontology?
-  label_names_complete_in_onto = [l for l in label_names_complete if graph.node[l]['namespace'] == onto ]
-
   ## COMMENT write out similar pairs
   fout = open("SimilarLabel.txt","w")
 
-  for index1, value1 in enumerate(tqdm(label_names_complete)): ## must do this to retain correct ordering
-
-    if value1 not in label_names_complete_in_onto:
-      continue ## keep only this ontology
+  for index1, value1 in enumerate(tqdm(names_in_label_vec)): ## @names_in_label_vec must do this to retain correct ordering
+    if value1 not in label_names_complete: ## only get sim of labels we want PSL to predict
+      continue
 
     # if index1 > 100:
     #   exit() ## testing
 
-    # get all ancestors (don't want get ancestors), we capture long-range similarity
-    ancestors = networkx.descendants(graph, value1)
+    ancestors = networkx.descendants(graph, value1) # don't want get ancestors, we capture long-range similarity
     where1 = np.where ( sim[index1] > 0.80 )[0]
     if len(where1) > 1:
       for w in where1:
         if w == index1: ## don't compare to self.
           continue
         value2 = index_label_map[w]
-        if graph.node[value2]['namespace'] != onto: ## split by ontology
+        if value2 not in label_names_complete: ## only get sim of labels we want PSL to predict
+          continue
+        if graph.node[value2]['namespace'] != ONTO: ## split by ontology
           continue
         if value2 not in ancestors: ## ancestor will be accounted for by subset-rule
           fout.write(value1+"\t"+value2+"\t"+str( np.round ( sim[index1][w], 6 ) )+"\n")
 
   fout.close()
+
+
+
+if len(sys.argv)<1: ## run script
+	print("Usage: \n")
+	sys.exit(1)
+else:
+  # WORKDIR,OUTPUT_PATH,LABEL_SEEN,LABEL_COMPLETE,ONTO
+	run ( sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6] )
+
 
 
 # GO:0000001      GO:0000266      0.8350666279101928
