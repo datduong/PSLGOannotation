4ba6d34da9c3512626b5a23519668f8d9870db62
Dat Duong <datd1988@gmail.com>
2019-12-10 04:44:10 -0800

    Update RunPSL.sh

--- MakeData/FormatData/RunPSL/RunPSL.sh
+++ MakeData/FormatData/RunPSL/RunPSL.sh
@@ -1,8 +1,13 @@
 
 
+#!/bin/bash
+. /u/local/Modules/default/init/modules.sh
+module load java
 
 cd /u/scratch/d/datduong/deepgo/data/PSLdata/molecular_function
 scp /u/scratch/d/datduong/PSLGOannotation/MakeData/ExampleCode/psl-cli-2.1.0.jar .
+scp /u/scratch/d/datduong/PSLGOannotation/MakeData/FormatData/RunPSL/input.data .
+scp /u/scratch/d/datduong/PSLGOannotation/MakeData/FormatData/RunPSL/rule.psl .
 
 # for file in Sample SimilarLabel Subset Label ErrorLink ClassifierLink Classifier ClassifierPredict Classifier_target Rating_truth ; do
 # perl -p -i -e 's/ /\t/g' $file.txt
@@ -12,4 +17,5 @@ scp /u/scratch/d/datduong/PSLGOannotation/MakeData/ExampleCode/psl-cli-2.1.0.jar
 java -jar psl-cli-2.1.0.jar --learn --model rule.psl --data input.data
 
 ## inference
-java -jar psl-cli-2.1.0.jar --infer --model rule-learned.psl --data input.data --output scratch_testing
+java -jar psl-cli-2.1.0.jar --infer --model rule-learned.psl --data input.data --output Prediction
+
